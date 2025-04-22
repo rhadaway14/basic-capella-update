057c7e32da7a8a4a7e2ffe8c44c72daa890ec2da
Robert Hadaway <rhadaway14@gmail.com>
2025-04-22 11:06:40 -0600

    read doc

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import datetime
+import json
 from couchbase.auth import PasswordAuthenticator
 from couchbase.cluster import Cluster
+from couchbase.exceptions import DocumentNotFoundException
 from couchbase.options import (
     ClusterOptions,
     UpsertOptions,
@@ -13,41 +15,55 @@ PASSWORD = "Password1!"
 BUCKET = "test"
 SCOPE = "test"
 COLLECTION = "test"
-KEY = "101"
-NEW_TTL_UNIX = 1751328000
+# KEY = "101"
+# NEW_TTL_UNIX = 1751328000
 
 
-# connect
-cluster = Cluster(CAPELLA_ENDPOINT, ClusterOptions(PasswordAuthenticator(USERNAME, PASSWORD)))
-bucket = cluster.bucket(BUCKET)
-collection = bucket.scope(SCOPE).collection(COLLECTION)
-print(collection)
+def update_ttl(key, exp):
+    try:
+        # connect
+        cluster = Cluster(CAPELLA_ENDPOINT, ClusterOptions(PasswordAuthenticator(USERNAME, PASSWORD)))
+        bucket = cluster.bucket(BUCKET)
+        collection = bucket.scope(SCOPE).collection(COLLECTION)
+        # print(collection)
 
-# get doc before
-before = collection.get(KEY, GetOptions(with_expiry=True))
-doc = before.content_as[dict]
-print(doc)
-print("ttl:", before.expiry_time)
+        # get doc before
+        before = collection.get(key, GetOptions(with_expiry=True))
+        doc = before.content_as[dict]
+        print(doc)
+        print("ttl:", before.expiry_time)
+
+        # edit time
+        now_utc = datetime.datetime.now(datetime.UTC)
+        expiration_datetime = datetime.datetime.fromtimestamp(exp, datetime.UTC)
+        ttl_delta = expiration_datetime - now_utc
+
+        # update
+        if ttl_delta.total_seconds() <= 0:
+            collection.remove(key)
+        else:
+            collection.upsert(key, doc, UpsertOptions(expiry=ttl_delta))
+            print(f"Updated TTL to expire in {ttl_delta.total_seconds():.0f} seconds")
+            print(f"Expires at: {expiration_datetime} UTC")
+
+            # get doc after
+            after = collection.get(key, GetOptions(with_expiry=True))
+            print("Updated document content:", after.content_as[dict])
+            print("Updated TTL (expiration time):", after.expiry_time)
+    except DocumentNotFoundException:
+        print("not found")
 
 
-# edit time
-now_utc = datetime.datetime.now(datetime.UTC)
-expiration_datetime = datetime.datetime.fromtimestamp(NEW_TTL_UNIX, datetime.UTC)
-ttl_delta = expiration_datetime - now_utc
+with open("expiration_tsky-all.json", "r") as f:
+    content = json.load(f)
 
-# update
-if ttl_delta.total_seconds() <= 0:
-    collection.remove(KEY)
-else:
-    collection.upsert(KEY, doc, UpsertOptions(expiry=ttl_delta))
-    print(f"Updated TTL to expire in {ttl_delta.total_seconds():.0f} seconds")
-    print(f"    → Expires at: {expiration_datetime} UTC")
+for item in content["rows"]:
+    print(item["key"])
+    print(item["value"])
+    update_ttl(item["key"], item["value"])
 
 
-# get doc after
-after = collection.get(KEY, GetOptions(with_expiry=True))
-print("Updated document content:", after.content_as[dict])
-print("Updated TTL (expiration time):", after.expiry_time)
+
 
 
 
